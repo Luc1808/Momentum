3f4f2e5e41d2eb69a7095159c3a3e4454e7f02b4
Luc1808 <520351@student.fontys.nl>
2026-03-28 19:56:32 +0100

    redisgn db

---
+++ src/main/java/io/github/Luc1808/momentum/service/ActivityService.java
@@ -0,0 +1,8 @@
+package io.github.Luc1808.momentum.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ActivityService {
+
+}
--- src/main/resources/db/migration/V1__create_user_table.sql
+++ src/main/resources/db/migration/V1__create_user_table.sql
@@ -9,21 +9,28 @@ CREATE TABLE users (
     email varchar(45) NOT NULL
 );
 
-CREATE TABLE activity (
-      id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-      title VARCHAR(45),
-      user_id UUID NOT NULL,
-      achieved BOOLEAN NOT NULL,
-      number_measure INTEGER,
-      created_at TIMESTAMP NOT NULL,
-      FOREIGN KEY (user_id) REFERENCES users(id)
+CREATE TABLE goal (
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    user_id UUID NOT NULL,
+    title TEXT NOT NULL,
+    session_per_week INTEGER NOT NULL,
+    deadline_type VARCHAR(10) NOT NULL CHECK ( deadline_type IN ('week', 'month', '3months') ),
+    start_date TIMESTAMP NOT NULL,
+    end_date TIMESTAMP NOT NULL,
+    created_at TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
-CREATE TABLE goal (
-      id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-      activity_id UUID NOT NULL,
-      achieved BOOLEAN NOT NULL,
-      number_target INTEGER NOT NULL,
-      deadline TIMESTAMP NOT NULL,
-      FOREIGN KEY (activity_id) REFERENCES activity(id)
+CREATE TABLE activity (
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    goal_id UUID NOT NULL,
+    user_id UUID NOT NULL,
+    act_date TIMESTAMP,
+    did_it BOOLEAN NOT NULL,
+    dur_min INTEGER,
+    notes VARCHAR(200),
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (goal_id) REFERENCES goal(id)
 );
